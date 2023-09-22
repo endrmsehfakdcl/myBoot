84984e5361ec648fed573aaa7809546d4692abc5
endrmsehfakdcl <toyandy0205@gmail.com>
2023-09-22 11:48:24 +0900

    queryDsl

--- shop/src/main/java/com/keduit/repository/ItemRepository.java
+++ shop/src/main/java/com/keduit/repository/ItemRepository.java
@@ -4,11 +4,13 @@ import com.keduit.entity.Item;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>,
+        QuerydslPredicateExecutor<Item> {
 
     List<Item> findByItemNm(String itemNm);
 
@@ -23,9 +25,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
     List<Item> findByItemByNative(@Param("itemDetail") String detail);
-
-
-
 
 
 }
--- shop/src/test/java/com/keduit/repository/ItemRepositoryTest.java
+++ shop/src/test/java/com/keduit/repository/ItemRepositoryTest.java
@@ -2,6 +2,9 @@ package com.keduit.repository;
 
 import com.keduit.constant.ItemSellStatus;
 import com.keduit.entity.Item;
+import com.keduit.entity.QItem;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +14,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,6 +29,9 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -50,13 +59,18 @@ class ItemRepositoryTest {
     }
 
     private void createItemList() {
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < 21; i++) {
             Item item = new Item();
             item.setItemNm("테스트 상품" + i);
             item.setPrice(10000 + i);
             item.setItemDetail("상세 정보" + i);
-            item.setItemSellStatus(ItemSellStatus.SELL);
-            item.setStockNumber(10000);
+            if (i < 11) {
+                item.setItemSellStatus(ItemSellStatus.SELL);
+                item.setStockNumber(10000);
+            } else {
+                item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+                item.setStockNumber(0);
+            }
             item.setRegTime(LocalDateTime.now());
             item.setUpdateTime(LocalDateTime.now());
 
@@ -96,7 +110,7 @@ class ItemRepositoryTest {
     @DisplayName("@query를 이용한 상품조회 테스트")
     public void findByItemDetailTest() {
         this.createItemList();
-        Pageable pageable = PageRequest.of(0,10, Sort.by("price").descending());
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("price").descending());
         List<Item> itemList = itemRepository.findByItemDetail("상세", pageable);
         for (Item item : itemList) {
             System.out.println("item = " + item);
@@ -156,19 +170,72 @@ class ItemRepositoryTest {
         Page<Item> result = itemRepository.findAll(pageable);
         System.out.println("result = " + result);
         System.out.println(" ================================= ");
-        for(Item item: result.getContent()) {
+        for (Item item : result.getContent()) {
             System.out.println("item = " + item);
         }
     }
 
     @Test
-    public void testSort(){
+    public void testSort() {
         Sort sort1 = Sort.by("id").descending();
-        Pageable pageable = PageRequest.of(0,10, sort1);
+        Pageable pageable = PageRequest.of(0, 10, sort1);
         Page<Item> result = itemRepository.findAll(pageable);
         result.get().forEach(item -> {
             System.out.println("item = " + item);
         });
     }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        List<Item> list = queryFactory
+                .select(qItem)
+                .from(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "상세" + "%"))
+                .orderBy(qItem.price.desc())
+                .fetch();
+        for (Item item : list) {
+            System.out.println("item = " + item);
+        }
+
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트2")
+    public void queryDslTest2() {
+        this.createItemList();
+
+        BooleanBuilder builder = new BooleanBuilder();
+        QItem qItem = QItem.item;
+        String itemDetail = "상세";
+        int price = 10001;
+        String itemSellStatus = "SELL";
+
+        builder.and(qItem.itemDetail.like("%" + itemDetail + "%"));
+        builder.and(qItem.price.gt(price));
+
+        if (StringUtils.equals(itemSellStatus, ItemSellStatus.SELL)) {
+            builder.and(qItem.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Item> result = itemRepository.findAll(builder, pageable);
+
+        System.out.println("전체 페이지 수 = " + result.getTotalPages());
+        System.out.println("조회한 전체 상품 수 = " + result.getTotalElements());
+        System.out.println("현재 페이지의 게시물 수 = " + result.getSize());
+        System.out.println("현재 페이지 번호 = " + result.getNumber());
+        System.out.println("content = " + result.getContent());
+
+        List<Item> list = result.getContent();
+        for(Item item : list) {
+            System.out.println("item = " + item);
+        }
+    }
+
 
 }
